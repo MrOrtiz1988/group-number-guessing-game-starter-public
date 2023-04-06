afd6b4914ad6b91616d9a0b67ea6cc335e55611b
Kobi Jones <kobijonesmail@gmail.com>
2023-04-06 15:53:27 -0500

    Add most of base functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,69 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
+  $('#submit-button').on('click', handleSubmit);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+
+  const player1Guess = Number($('#player1-guess').val());
+  const player2Guess = Number($('#player2-guess').val());
+
+  const guesses = {
+    player1: player1Guess,
+    player2: player2Guess
+  }
+
+  $('#player1-guess').val('');
+  $('#player2-guess').val('');
+
+  $.ajax({
+    method: 'POST',
+    url: '/guess',
+    data: guesses
+  }).then(response => {
+    console.log('POST response:', response);
+
+    $.ajax({
+      method: 'GET',
+      url: '/guess'
+    }).then(response => {
+      console.log('GET response:', response);
+      // if player1 won or player2 won
+      //    then append reset button
+      //    tell server to pick new random number
+      $('#result').text(response.winner);
+      $('#rounds').text(response.totalGuesses);
+      $('#history').empty();
+      for(const index in response.guesses) {
+        $('#history').append(`
+        <tr>
+          <td>${Number(index) + 1}</td>
+          <td>${response.guesses[index].player1} ${response.differences[index].player1}</td>
+          <td>${response.guesses[index].player2} ${response.differences[index].player2}</td>
+        </tr>
+        `)
+      }
+
+      if (
+        response.differences[response.differences.length - 1].player1 === 'winner' ||
+        response.differences[response.differences.length - 1].player2 === 'winner'
+      ) {
+        $('#reset-button').removeAttr('disabled');
+      }
+
+
+      // new
+      // for (const [index, guess] of response.guesses.entries()) {
+      //   $('#history').append(`
+      //   <tr>
+      //     <td>${index + 1}</td>
+      //     <td>${guess.player1} ${response.difference.player1}</td>
+      //     <td>${guess.player2} ${response.difference.player2}</td>
+      //   </tr>
+      //   `)
+      // }
+    });
+  });
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+let randomNumber = generateRandomNumber(1, 25);
+const guesses = [];
+
+console.log(randomNumber);
+
+function calculate() {
+  const differences = [];
+  for (const guess of guesses) {
+    const difference = {
+      player1: 'winner',
+      player2: 'winner'
+    };
+    if (Number(guess.player1) > randomNumber) {
+      difference.player1 = 'too high';
+    } else if (Number(guess.player1) < randomNumber) {
+      difference.player1 = 'too low';
+    }
+
+    if (Number(guess.player2) > randomNumber) {
+      difference.player2 = 'too high';
+    } else if (Number(guess.player2) < randomNumber) {
+      difference.player2 = 'too low';
+    }
+
+    differences.push(difference);
+  }
+
+  return differences;
+}
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -10,8 +44,38 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.post('/guess', (req, res) => {
+  const guess = req.body;
+  guesses.push(guess);
+  console.log(guess);
+  res.sendStatus(201);
+});
 
+app.get('/guess', (req, res) => {
+  if (guesses.length === 0) {
+    res.send('No guesses');
+    return;
+  }
+
+  let winner;
+
+  const lastGuess = guesses[guesses.length - 1];
+  if (Number(lastGuess.player1) === randomNumber) {
+    winner = 'Player 1 won!';
+  } else if (Number(lastGuess.player2) === randomNumber) {
+    winner = 'Player 2 won!';
+  } else {
+    winner = 'No winner';
+  }
+
+  res.send({
+    winner,
+    totalGuesses: guesses.length,
+    guesses: guesses,
+    differences: calculate()
+  });
+});
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
+  console.log (`Server is running at http://localhost:${PORT}`)
 })
